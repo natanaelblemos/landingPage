9ad7ddf9cf0ab80c20ca7651dcdfef447a432fb4
Natanael Basile <natanaelb.lemos@gmail.com>
2021-07-17 00:00:05 -0300

    ultimas edições *espero*

--- js/action.js
+++ js/action.js
@@ -2,14 +2,23 @@
 function pegaEmail(){
     let inputEmail = document.querySelector('#email');
         let contato = inputEmail.value;
+        inputEmail.className = '';
         //console.log(contato);
         return contato;
 }
 
 function gravaEmail(contato){
     localStorage.setItem('contato',JSON.stringify(contato));
+        let inputEmail = document.querySelector('#email');
+        inputEmail.value = '';
+    if(contato.match(/@/)){
+        let contatos =JSON.parse(localStorage.getItem('contato'));
+        inputEmail.className = '';
+        console.log(contatos);
+    }else{
+        alert('Este email não é válido!');
+        inputEmail.className = 'inputErro';
+    }
 
-    let contatos =JSON.parse(localStorage.getItem('contato'));
-         console.log(contatos)
      
 }
